972beabde0ecd83c040d210281a8351922b6897e
Matt Adams <mrwadams@gmail.com>
2024-04-27 16:10:33 +0100

    Rename Welcome.py

--- 0_👋_Welcome.py
+++ 0_👋_Welcome.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2023, Matthew Adams
+Copyright (C) 2024, Matthew Adams
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
